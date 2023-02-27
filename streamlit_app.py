5aae92652a3fe5e0fe45d4aa4a7983856cd2e6ce
workgithub04 <126447295+workgithub04@users.noreply.github.com>
2023-02-27 14:03:22 +0530

    Update streamlit_app.py
    added new lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+
