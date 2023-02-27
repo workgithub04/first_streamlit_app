134cbd6d46f4b80907570da05f172fa564ed548a
workgithub04 <126447295+workgithub04@users.noreply.github.com>
2023-02-27 14:04:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,6 @@ streamlit.header('Breakfast menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
 
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+
